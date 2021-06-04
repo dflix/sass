53a92582d75fc893497f607962139d51d614d4b1
Marcio Leite <mbleusou@gmail.com>
2021-06-03 21:58:07 -0300

    Documentação Saas

---
+++ index.php
@@ -0,0 +1,49 @@
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sass</title>
+        <link rel="stylesheet" href="_cdn/css/style.css" />
+    </head>
+    <body>
+        
+        <div class="main_header">
+        
+            <nav class="main_header_navigation"> 
+                <ul> 
+                    <li> <a href="#">Home </a> </li>
+                    <li> <a href="#">Portifolio </a> </li>
+                    <li> <a href="#">Quem Somos </a> </li>
+                    <li> <a href="#">Contato </a> </li>
+                </ul>
+            </nav>
+        
+        </div>
+        
+        <div class="main_site"> 
+        
+            <div class="main_site_content"> 
+                <h1> Teste de Conteudo </h1>
+                
+                <h2 class="title-blue"> Teste</h2>
+                <h2 class="title-yellow"> Teste</h2>
+                <h2 class="title-green"> Teste</h2>
+                <h2 class="title-red"> Teste</h2>
+                
+                <button class="btn btn-green btn-large"> Clique Aqui </button>
+                <button class="btn btn-blue btn-large"> Clique Aqui </button>
+                <button class="btn btn-yellow btn-large"> Clique Aqui </button>
+                <button class="btn btn-red btn-large"> Clique Aqui </button>
+                <button class="btn btn-orange btn-large"> Clique Aqui </button>
+            </div>
+            
+        </div>
+        
+        <div class="main_footer"> 
+            <div class="main_footer_content"> 
+                <p> Todos os Direitos são reservados</p>
+            </div>
+        </div>
+            
+    </body>
+</html>
